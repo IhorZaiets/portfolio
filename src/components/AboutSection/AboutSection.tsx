df75fe3219007503faca82fe8a23bcf096e60926
IhorZaiets <ihor.zaiets2001@gmail.com>
2022-10-27 00:08:08 +0300

    fix probem with img import

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutSection: React.FC = () => (
   <section>
     <figure>
-      <img src="../../assets/Logo.svg" alt="about me" />
+      <img src={require('../../assets/AboutPhoto.jpeg')} alt="about me" />
     </figure>
     <article>
       <h2>About me</h2>
