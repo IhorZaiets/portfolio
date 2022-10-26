5fa6b3dce7bc25f49225d97f7d3616d231e9bc8a
IhorZaiets <ihor.zaiets2001@gmail.com>
2022-10-26 22:35:40 +0300

    correct main section and start about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+
 import './App.css';
+
+import AboutSection from './components/AboutSection/AboutSection';
 import Header from './components/header/Header';
 import MainSection from './components/MainSection/MainSection';
 
 const App: React.FC = () => (
   <>
-    <MainSection />
+    <Header />
+    <main>
+      <MainSection />
+      <AboutSection />
+    </main>
   </>
 );
 
---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutSection: React.FC = () => (
+  <section>
+    <figure>
+      <img src="../../assets/Logo.svg" alt="about me" />
+    </figure>
+    <article>
+      <h2>About me</h2>
+    </article>
+  </section>
+);
+
+export default AboutSection;
